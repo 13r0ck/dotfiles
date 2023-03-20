cf1cbbe28c0be34535f79106be07eb54dbe977fc
13r0ck <brock@szu.email>
2023-03-19 19:04:13 -0600

    scripts/Nextcloud add option to install to different location

--- scripts/Nextcloud.sh
+++ scripts/Nextcloud.sh
@@ -9,6 +9,7 @@ DOMAIN=""
 EMAIL=""
 USERNAME=""
 PASSWORD=""
+DATA_LOCATION=""
 
 ## END CONFIG
 
@@ -19,27 +20,42 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 echo "Make sure you have set your IaaS prover's firewall settings to allow port 80 and 443."
-read -p "Press [ENTER] to continue..." null
+read -pr "Press [ENTER] to continue..." null
 echo "You need a domain pointed at your server. An IP address isn't good enough for this script."
-read -p "Press [ENTER] to continue..." null
+read -pr "Press [ENTER] to continue..." null
+
+echo "Use alternate directory?"
+echo "If using /script/BlockStorage.sh this value is '/mnt/blockstorage/nextcloud'"
+read -pr "USe alternate data directory? (Path in '/mnt' or empty to use default): " DATA_LOCATION
 
 if [ -z "$DOMAIN" ];then
-  read -p "Domain (example.com): " DOMAIN
+  read -pr "Domain (example.com): " DOMAIN
 fi
 
 if [ -z "$EMAIL" ]; then
-  read -p "Email (email@example.com): " EMAIL
+  read -pr "Email (email@example.com): " EMAIL
 fi
 
 if [ -z "$USERNAME" ]; then
-  read -p "Nextcloud Admin username: " USERNAME
+  read -pr "Nextcloud Admin username: " USERNAME
 fi
 
 if [ -z "$PASSWORD" ]; then
-  read -p "Nextcloud admin password: " PASSWORD
+  read -pr "Nextcloud admin password: " PASSWORD
 fi
 
 sudo snap install nextcloud
+
+if [ -z "$DATA_LOCATION" ]; then
+  sudo snap connect nextcloud:removable-media
+  mkdir -p "$DATA_LOCATION"
+  sed -i "s/'directory' => getenv('NEXTCLOUD_DATA_DIR'),/'directory' => '${DATA_LOCATION}',/" \
+    /var/snap/nextcloud/current/nextcloud/config/autoconfig.php
+  sudo mkdir -p "$DATA_LOCATION"
+  sudo chown -R root:root "$DATA_LOCATION"
+  sudo chmod 0770 "$DATA_LOCATION"
+fi
+
 sudo nextcloud.manual-install "$USERNAME" "$PASSWORD"
 sudo nextcloud.occ config:system:set trusted_domains 1 --value="$DOMAIN"
 sudo ufw allow 80,443/tcp
