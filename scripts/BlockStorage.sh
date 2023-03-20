6d2a4f0a2faa7b8141d9536bc16bca24c7239cc0
13r0ck <brock@szu.email>
2023-03-19 18:27:38 -0600

    scripts/BlockStorage: fix syntax

--- scripts/BlockStorage.sh
+++ scripts/BlockStorage.sh
@@ -5,22 +5,22 @@
 
 ## CONFIG
 
-PATH="/dev/vdb"
+BLK="/dev/vdb"
 
 ## END CONFIG
 
 # Check if script is being run as root or with sudo
-if [ $(id -u) -ne 0 ]; then
+if [ "$(id -u)" -ne 0 ]; then
     echo "This script must be run as root or with sudo"
     exit 1
 fi
 
 echo "Make sure your have attached the Block Storage to this server in the Vultr management console."
-read -p "Press [ENTER] to continue..."
+read -p "Press [ENTER] to continue..." null
 
-parted -s /dev/vdb mklabel gpt
-parted -s /dev/vdb unit mib mkpart primary 0% 100%
-mkfs.ext4 /dev/vdb1
+parted -s $BLK mklabel gpt
+parted -s $BLK unit mib mkpart primary 0% 100%
+mkfs.ext4 "${BLK}1"
 mkdir /mnt/blockstorage
 echo /dev/vdb1 /mnt/blockstorage ext4 defaults,noatime,nofail 0 0 >> /etc/fstab
 mount /mnt/blockstorage
